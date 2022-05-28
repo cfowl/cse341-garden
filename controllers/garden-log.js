aee16463d443a46ebfb3e85f283042d02a29d064
cfowl <goose_7@msn.com>
2022-05-28 14:19:33 -0600

    added try/catch to controllers

--- controllers/garden-log.js
+++ controllers/garden-log.js
@@ -3,85 +3,105 @@ const ObjectId = require('mongodb').ObjectId;
 
 // gets all garden log entries
 async function getAllEntries(req, res) {
-    mongodb
-      .getDb()
-      .db()
-      .collection('garden-log')
-      .find()
-      .toArray((err, data) => {
-        if(err) res.status(400).json({message: err});
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data);
-      });
+    try {
+        mongodb
+          .getDb()
+          .db()
+          .collection('garden-log')
+          .find()
+          .toArray((err, data) => {
+            if(err) res.status(400).json({message: err});
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data);
+          });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
   
 // gets one garden log entry by ID
 async function getOneEntry(req, res) {
     if (!ObjectId.isValid(req.params.id)) res.status(400).json('Must use a valid garden log ID to find an entry.');
 
-    const entryId = new ObjectId(req.params.id);
-    mongodb
-      .getDb()
-      .db()
-      .collection('garden-log')
-      .find({_id: entryId})
-      .toArray((err, data) => {
-        if(err) res.status(400).json({message: err});
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data[0]);
-      });
+    try {
+        const entryId = new ObjectId(req.params.id);
+        mongodb
+          .getDb()
+          .db()
+          .collection('garden-log')
+          .find({_id: entryId})
+          .toArray((err, data) => {
+            if(err) res.status(400).json({message: err});
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data[0]);
+          });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // creates a new garden log entry
 async function createEntry(req, res) {
-    const entry = {
-        year: req.body.year,
-        seedName: req.body.seedName,
-        seedType: req.body.seedType,
-        variety: req.body.variety,
-        plantedQty: req.body.plantedQty,
-        plantedDate: req.body.plantedDate,
-        harvestedQty: req.body.harvestedQty,
-        harvestedDate: req.body.harvestedDate,
-        notes: req.body.notes
-    };
-    const result = await mongodb.getDb().db().collection('garden-log').insertOne(seedInfo);
-    console.log(result);
-    if(result.acknowledged) res.status(201).json(result);
-    else res.status(500).json(result.error || `An error occurred while inserting the garden log entry.`);
+    try {
+        const entry = {
+            year: req.body.year,
+            seedName: req.body.seedName,
+            seedType: req.body.seedType,
+            variety: req.body.variety,
+            plantedQty: req.body.plantedQty,
+            plantedDate: req.body.plantedDate,
+            harvestedQty: req.body.harvestedQty,
+            harvestedDate: req.body.harvestedDate,
+            notes: req.body.notes
+        };
+        const result = await mongodb.getDb().db().collection('garden-log').insertOne(seedInfo);
+        console.log(result);
+        if(result.acknowledged) res.status(201).json(result);
+        else res.status(500).json(result.error || `An error occurred while inserting the garden log entry.`);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // updates one garden log entry by ID
 async function updateEntry(req, res) {
     if (!ObjectId.isValid(req.params.id)) res.status(400).json('Must use a valid garden log ID to update an entry.');
     
-    const entryId = new ObjectId(req.params.id);
-    const entry = {
-        year: req.body.year,
-        seedName: req.body.seedName,
-        seedType: req.body.seedType,
-        variety: req.body.variety,
-        plantedQty: req.body.plantedQty,
-        plantedDate: req.body.plantedDate,
-        harvestedQty: req.body.harvestedQty,
-        harvestedDate: req.body.harvestedDate,
-        notes: req.body.notes
-    };
-    const result = await mongodb.getDb().db().collection('garden-log').replaceOne({_id: entryId}, entry);
-    console.log(result);
-    if(result.modifiedCount > 0) res.status(204).send();
-    else res.status(500).json(result.error || `An error occurred while updating the garden log entry.`);
+    try {
+        const entryId = new ObjectId(req.params.id);
+        const entry = {
+            year: req.body.year,
+            seedName: req.body.seedName,
+            seedType: req.body.seedType,
+            variety: req.body.variety,
+            plantedQty: req.body.plantedQty,
+            plantedDate: req.body.plantedDate,
+            harvestedQty: req.body.harvestedQty,
+            harvestedDate: req.body.harvestedDate,
+            notes: req.body.notes
+        };
+        const result = await mongodb.getDb().db().collection('garden-log').replaceOne({_id: entryId}, entry);
+        console.log(result);
+        if(result.modifiedCount > 0) res.status(204).send();
+        else res.status(500).json(result.error || `An error occurred while updating the garden log entry.`);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // deletes a garden log entry by ID
 async function deleteEntry(req, res) {
     if (!ObjectId.isValid(req.params.id)) res.status(400).json('Must use a valid garden log ID to delete an entry.');
     
-    const entryId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('garden-log').deleteOne({_id: entryId});
-    console.log(result);
-    if(result.deletedCount > 0) res.status(204).send();
-    else res.status(500).json(result.error || `An error occurred while deleting the garden log entry.`);
+    try {
+        const entryId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().db().collection('garden-log').deleteOne({_id: entryId});
+        console.log(result);
+        if(result.deletedCount > 0) res.status(204).send();
+        else res.status(500).json(result.error || `An error occurred while deleting the garden log entry.`);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 module.exports = {
--- controllers/seeds.js
+++ controllers/seeds.js
@@ -3,87 +3,107 @@ const ObjectId = require('mongodb').ObjectId;
 
 // gets all seeds' info
 async function getAllSeeds(req, res) {
-    mongodb
-      .getDb()
-      .db()
-      .collection('seeds')
-      .find()
-      .toArray((err, data) => {
-        if(err) res.status(400).json({message: err});
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data);
-      });
+    try {
+        mongodb
+          .getDb()
+          .db()
+          .collection('seeds')
+          .find()
+          .toArray((err, data) => {
+            if(err) res.status(400).json({message: err});
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data);
+          });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
   
 // gets one seed's info by ID
 async function getOneSeed(req, res) {
     if (!ObjectId.isValid(req.params.id)) res.status(400).json('Must use a valid seed ID to find a seed\'s info.');
 
-    const seedId = new ObjectId(req.params.id);
-    mongodb
-      .getDb()
-      .db()
-      .collection('seeds')
-      .find({_id: seedId})
-      .toArray((err, data) => {
-        if(err) res.status(400).json({message: err});
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data[0]);
-      });
+    try {
+        const seedId = new ObjectId(req.params.id);
+        mongodb
+          .getDb()
+          .db()
+          .collection('seeds')
+          .find({_id: seedId})
+          .toArray((err, data) => {
+            if(err) res.status(400).json({message: err});
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(data[0]);
+          });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // creates a new seed's info
 async function createSeed(req, res) {
-    const seedInfo = {
-        name: req.body.name,
-        type: req.body.type,
-        seedsPerOunce: req.body.seedsPerOunce,
-        seedsPer100Feet: req.body.seedsPer100Feet,
-        sowingDepth: req.body.sowingDepth,
-        daysToGermination: req.body.daysToGermination,
-        rowSpacing: req.body.rowSpacing,
-        plantSpacing: req.body.plantSpacing,
-        daysToHarvest: req.body.daysToHarvest,
-        plantingDate: req.body.plantingDate
-    };
-    const result = await mongodb.getDb().db().collection('seeds').insertOne(seedInfo);
-    console.log(result);
-    if(result.acknowledged) res.status(201).json(result);
-    else res.status(500).json(result.error || `An error occurred while inserting the seed's info`);
+    try {
+        const seedInfo = {
+            name: req.body.name,
+            type: req.body.type,
+            seedsPerOunce: req.body.seedsPerOunce,
+            seedsPer100Feet: req.body.seedsPer100Feet,
+            sowingDepth: req.body.sowingDepth,
+            daysToGermination: req.body.daysToGermination,
+            rowSpacing: req.body.rowSpacing,
+            plantSpacing: req.body.plantSpacing,
+            daysToHarvest: req.body.daysToHarvest,
+            plantingDate: req.body.plantingDate
+        };
+        const result = await mongodb.getDb().db().collection('seeds').insertOne(seedInfo);
+        console.log(result);
+        if(result.acknowledged) res.status(201).json(result);
+        else res.status(500).json(result.error || `An error occurred while inserting the seed's info`);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // updates one seed's info by ID
 async function updateSeed(req, res) {
     if (!ObjectId.isValid(req.params.id)) res.status(400).json('Must use a valid seed ID to update a seed\'s info.');
     
-    const seedId = new ObjectId(req.params.id);
-    const seedInfo = {
-        name: req.body.name,
-        type: req.body.type,
-        seedsPerOunce: req.body.seedsPerOunce,
-        seedsPer100Feet: req.body.seedsPer100Feet,
-        sowingDepth: req.body.sowingDepth,
-        daysToGermination: req.body.daysToGermination,
-        rowSpacing: req.body.rowSpacing,
-        plantSpacing: req.body.plantSpacing,
-        daysToHarvest: req.body.daysToHarvest,
-        plantingDate: req.body.plantingDate
-    };
-    const result = await mongodb.getDb().db().collection('seeds').replaceOne({_id: seedId}, seedInfo);
-    console.log(result);
-    if(result.modifiedCount > 0) res.status(204).send();
-    else res.status(500).json(result.error || `An error occurred while updating the seed's info`);
+    try {
+        const seedId = new ObjectId(req.params.id);
+        const seedInfo = {
+            name: req.body.name,
+            type: req.body.type,
+            seedsPerOunce: req.body.seedsPerOunce,
+            seedsPer100Feet: req.body.seedsPer100Feet,
+            sowingDepth: req.body.sowingDepth,
+            daysToGermination: req.body.daysToGermination,
+            rowSpacing: req.body.rowSpacing,
+            plantSpacing: req.body.plantSpacing,
+            daysToHarvest: req.body.daysToHarvest,
+            plantingDate: req.body.plantingDate
+        };
+        const result = await mongodb.getDb().db().collection('seeds').replaceOne({_id: seedId}, seedInfo);
+        console.log(result);
+        if(result.modifiedCount > 0) res.status(204).send();
+        else res.status(500).json(result.error || `An error occurred while updating the seed's info`);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 // deletes a seed's info by ID
 async function deleteSeed(req, res) {
     if (!ObjectId.isValid(req.params.id)) res.status(400).json('Must use a valid seed ID to delete a seed\'s info.');
     
-    const seedId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('seeds').deleteOne({_id: seedId});
-    console.log(result);
-    if(result.deletedCount > 0) res.status(204).send();
-    else res.status(500).json(result.error || `An error occurred while deleting the  seed's info`);
+    try {
+        const seedId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().db().collection('seeds').deleteOne({_id: seedId});
+        console.log(result);
+        if(result.deletedCount > 0) res.status(204).send();
+        else res.status(500).json(result.error || `An error occurred while deleting the  seed's info`);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 }
 
 module.exports = {
