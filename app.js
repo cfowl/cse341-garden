48ae6c7cad57afeb38f1493b44f6e5e6682cf3e4
cfowl <goose_7@msn.com>
2022-05-28 12:50:42 -0600

    added uncaught error handling to app

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(bodyParser.json())
-   .use((req, res, next) => {
-       res.setHeader('Access-Control-Allow-Origin', '*');
-       res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-       next();
-    })
-   .use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+  .use('/', require('./routes'));
+
+// handle any uncaught errors
+process.on('uncaughtException', (err, origin) => {
+console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
 
 // connect to the server and start the app
 mongodb.initDb(err => {
